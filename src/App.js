7af66f962b91a0031b5756e10fe9211a4c648114
alejandroruizdiaz <alee_ruizdiaz@hotmail.com>
2022-11-08 14:55:49 -0300

    adding router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './routes/Home.js'
+import About from './routes/About.js'
+import Project from './routes/Project.js'
+import Contact from './routes/Contact.js'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/project' element={<Project/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='*' element={<h1>ERROR 404</h1>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
