ad8e5ad48c07b96e2efc387eb1048b5898ee7336
alejandroruizdiaz <alee_ruizdiaz@hotmail.com>
2022-12-05 22:34:08 -0300

    updating cv1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>alee_ruizdiaz@hotmail.com</h3>
+          <h3>Alee_ruizdiaz@hotmail.com</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Alejandro Ruiz Diaz</h3>
